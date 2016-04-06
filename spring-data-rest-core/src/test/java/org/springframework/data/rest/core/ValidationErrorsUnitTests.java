e4a98f76f9dd95789a09049e0eebb718921126dd
Björn Andersson <bandersson@pivotal.io>
2016-04-06 19:26:14 +0800

    Failing test for getting nested values

--- spring-data-rest-core/src/test/java/org/springframework/data/rest/core/ValidationErrorsUnitTests.java
+++ spring-data-rest-core/src/test/java/org/springframework/data/rest/core/ValidationErrorsUnitTests.java
@@ -22,7 +22,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.beans.NotReadablePropertyException;
 import org.springframework.data.keyvalue.core.mapping.context.KeyValueMappingContext;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
 /**
  * Unit tests for {@link ValidationErrors}.
@@ -48,11 +51,56 @@ public class ValidationErrorsUnitTests {
 		assertThat(errors.getFieldError().getField(), is("bars[0].field"));
 	}
 
+	@Test
+	public void getsTheNestedFieldsValue() {
+		ValidationErrors errors = new ValidationErrors(new Foo(), context.getPersistentEntity(Foo.class));
+
+		expectedErrorBehavior(errors);
+	}
+
+	@Test
+	public void getsTheNestedFieldsValueFromOther() {
+		Errors errors = new BeanPropertyBindingResult(new Foo(), "foo");
+
+		expectedErrorBehavior(errors);
+	}
+
+	private void expectedErrorBehavior(Errors errors) {
+		// Can access bars before path gets pushed
+		assertThat(errors.getFieldValue("bars"), is(notNullValue()));
+
+		errors.pushNestedPath("bars[0]");
+		Boolean errorThrown = false;
+		try {
+			// Can't access bars after path is pushed
+			Object value = errors.getFieldValue("bars");
+		} catch (NotReadablePropertyException e) {
+			errorThrown = true;
+		}
+		assertTrue("No missing bars field exception thrown", errorThrown);
+
+		// Can access field after being pushed into bars
+		Object value = errors.getFieldValue("field");
+		assertTrue(value != null && value.equals("Hello"));
+	}
+
 	static class Foo {
 		List<Bar> bars = new ArrayList<Bar>();
+
+		public Foo() {
+			bars.add(new Bar());
+		}
+
+		public List<Bar> getBars() {
+			return bars;
+		}
 	}
 
 	static class Bar {
-		String field;
+		String field = "Hello";
+
+		public String getField() {
+			return field;
+		}
 	}
 }
